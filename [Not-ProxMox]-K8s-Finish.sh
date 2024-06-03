733372a1268fcd42c850789f402f2f80115825f6
MaxU1301 <max@ullrichfamily.com>
2024-06-03 14:22:32 -0400

    Fixed Things

---
+++ [Not-ProxMox]-K8s-Finish.sh
@@ -0,0 +1,3 @@
+apt install kubeadm kubelet kubectl kubernetes-cni -y
+
+wget https://raw.githubusercontent.com/MaxU1301/Proxmox-Scripts/main/SubScripts/SetAsK8sMaster.sh
--- [Not-ProxMox]-K8s-Start.sh
+++ [Not-ProxMox]-K8s-Start.sh
@@ -1,16 +1,14 @@
 # Install initial packages
-apt install cifs-utils nfs-common
+apt install cifs-utils nfs-common -y
 add-apt-repository ppa:zhangsongcui3371/fastfetch
-apt install fastfetch
+apt install fastfetch -y
 
 # Install K8s Using kubeadm
 ufw disable
 swapoff -a; sed -i '/swap/d' /etc/fstab
 
-apt install docker.io apt-transport-https curl ca-certificates gpg
+apt install docker.io apt-transport-https curl ca-certificates gpg -y
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
-apt install kubeadm kubelet kubectl kubernetes-cni
-
-wget https://raw.githubusercontent.com/MaxU1301/Proxmox-Scripts/main/SubScripts/SetAsK8sMaster.sh
+wget https://raw.githubusercontent.com/MaxU1301/Proxmox-Scripts/main/%5BNot-ProxMox%5D-K8s-Finish.sh
