f9f8db217def989b774ba5433f8c9447f1c57099
MaxU1301 <max@ullrichfamily.com>
2024-06-03 14:29:02 -0400

    worked on not proxmox K8s Start

--- [Not-ProxMox]-K8s-Finish.sh
+++
@@ -1,3 +0,0 @@
-apt install kubeadm kubelet kubectl kubernetes-cni -y
-
-wget https://raw.githubusercontent.com/MaxU1301/Proxmox-Scripts/main/SubScripts/SetAsK8sMaster.sh
--- [Not-ProxMox]-K8s-Start.sh
+++ [Not-ProxMox]-K8s-Start.sh
@@ -11,4 +11,9 @@ apt install docker.io apt-transport-https curl ca-certificates gpg -y
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
-wget https://raw.githubusercontent.com/MaxU1301/Proxmox-Scripts/main/%5BNot-ProxMox%5D-K8s-Finish.sh
+apt update
+apt upgrade
+apt install kubeadm kubelet kubectl kubernetes-cni -y
+
+wget https://raw.githubusercontent.com/MaxU1301/Proxmox-Scripts/main/SubScripts/SetAsK8sMaster.sh
+sudo chmod +x SetAsK8sMaster.sh
