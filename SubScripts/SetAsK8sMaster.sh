831f4d6df4f272c4532cc1b502d7f25c6780e273
MaxU1301 <max@ullrichfamily.com>
2024-06-04 12:48:53 -0400

    updated calico installation in setask8smaster.sh

--- SubScripts/SetAsK8sMaster.sh
+++ SubScripts/SetAsK8sMaster.sh
@@ -11,9 +11,9 @@ sudo chown $(id -u):$(id -g) $HOME/.kube/config
 # Install Calico
 
 # sudo kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
-kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.28.0/manifests/tigera-operator.yaml
+sudo kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.28.0/manifests/tigera-operator.yaml
 curl https://raw.githubusercontent.com/projectcalico/calico/v3.28.0/manifests/custom-resources.yaml -O
-kubectl create -f custom-resources.yaml
+sudo kubectl create -f custom-resources.yaml
 watch kubectl get pods -n calico-system
 
 kubeadm token create --print-join-command
