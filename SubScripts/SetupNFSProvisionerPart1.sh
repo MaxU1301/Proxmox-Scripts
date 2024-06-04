bcf59c850860129e0f6910cc9cfbb5b1e2357f6a
MaxU1301 <max@ullrichfamily.com>
2024-06-04 13:32:09 -0400

    chaged github nfs location

--- SubScripts/SetupNFSProvisionerPart1.sh
+++ SubScripts/SetupNFSProvisionerPart1.sh
@@ -3,10 +3,15 @@ cd nfs-provisioner
 rm *
 
 # Download nfs-provisioner scripts
-wget https://raw.githubusercontent.com/justmeandopensource/kubernetes/master/yamls/nfs-provisioner/rbac.yaml
-wget https://raw.githubusercontent.com/justmeandopensource/kubernetes/master/yamls/nfs-provisioner/deployment.yaml
-wget https://raw.githubusercontent.com/justmeandopensource/kubernetes/master/yamls/nfs-provisioner/default-sc.yaml
-wget https://raw.githubusercontent.com/justmeandopensource/kubernetes/master/yamls/nfs-provisioner/class.yaml
+wget https://raw.githubusercontent.com/MaxU1301/Proxmox-Scripts/main/SubScripts/nfs-provisioner/rbac.yaml
+wget https://raw.githubusercontent.com/MaxU1301/Proxmox-Scripts/main/SubScripts/nfs-provisioner/deployment.yaml
+wget https://raw.githubusercontent.com/MaxU1301/Proxmox-Scripts/main/SubScripts/nfs-provisioner/default-sc.yaml
+wget https://raw.githubusercontent.com/MaxU1301/Proxmox-Scripts/main/SubScripts/nfs-provisioner/class.yaml
+
+# wget https://raw.githubusercontent.com/justmeandopensource/kubernetes/master/yamls/nfs-provisioner/rbac.yaml
+# wget https://raw.githubusercontent.com/justmeandopensource/kubernetes/master/yamls/nfs-provisioner/deployment.yaml
+# wget https://raw.githubusercontent.com/justmeandopensource/kubernetes/master/yamls/nfs-provisioner/default-sc.yaml
+# wget https://raw.githubusercontent.com/justmeandopensource/kubernetes/master/yamls/nfs-provisioner/class.yaml
 
 # Modify yaml files
 echo "edit deployment.yaml"
