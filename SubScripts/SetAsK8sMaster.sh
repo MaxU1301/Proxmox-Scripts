cdba5878306bacfce24b811a1048a969f015d815
MaxU1301 <max@ullrichfamily.com>
2024-06-04 15:41:52 -0400

    changed MetalLB setup

--- SubScripts/SetAsK8sMaster.sh
+++ SubScripts/SetAsK8sMaster.sh
@@ -11,17 +11,15 @@ sudo chown $(id -u):$(id -g) $HOME/.kube/config
 # Install Calico
 
 # sudo kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
-sudo kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.28.0/manifests/tigera-operator.yaml
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.28.0/manifests/tigera-operator.yaml
 curl https://raw.githubusercontent.com/projectcalico/calico/v3.28.0/manifests/custom-resources.yaml -O
-sudo kubectl create -f custom-resources.yaml
-watch kubectl get pods -n calico-system
+kubectl create -f custom-resources.yaml
+kubectl get pods -n calico-system
 
-sudo kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f https://docs.projectcalico.org/v3.14/manifests/calico.yaml
+# sudo kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f https://docs.projectcalico.org/v3.14/manifests/calico.yaml
 
 # Install helm
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
-
-kubeadm token create --print-join-command
 
 # Download MetalLB and NFS Scripts
 wget https://raw.githubusercontent.com/MaxU1301/Proxmox-Scripts/main/SubScripts/SetupMetalLB.sh
@@ -31,3 +29,4 @@ sudo chmod +x SetupMetalLB.sh
 sudo chmod +x SetupNFSProvisioner.sh
 
 echo "Please run SetupNFSProvisioner.sh and SetupMetalLB.sh"
+kubeadm token create --print-join-command
