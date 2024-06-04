50a1c7aae1642e9a0d6fef83ee29564373a3473b
MaxU1301 <max@ullrichfamily.com>
2024-06-04 16:21:26 -0400

    updated callico config

--- SubScripts/SetAsK8sMaster.sh
+++ SubScripts/SetAsK8sMaster.sh
@@ -12,7 +12,11 @@ sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 # sudo kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
 kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.28.0/manifests/tigera-operator.yaml
-kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.28.0/manifests/custom-resources.yaml
+kubectl create -f https://raw.githubusercontent.com/MaxU1301/Proxmox-Scripts/blob/main/SubScripts/CalicoConfig/custom-resources.yaml
+
+watch kubectl get pods -n calico-system
+kubectl taint nodes --all node-role.kubernetes.io/control-plane-
+kubectl get nodes -o wide
 
 # sudo kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f https://docs.projectcalico.org/manifests/calico.yaml
 
