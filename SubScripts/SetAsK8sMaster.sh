f66e6de5ea65f6d859fc88cd3bd1bed11af5318c
MaxU1301 <max@ullrichfamily.com>
2024-06-04 16:18:48 -0400

    updated callico config

--- SubScripts/SetAsK8sMaster.sh
+++ SubScripts/SetAsK8sMaster.sh
@@ -1,7 +1,7 @@
 sudo apt update
 sudo apt upgrade -y
 sudo apt install kubeadm kubelet kubectl kubernetes-cni
-sudo kubeadm init
+sudo kubeadm init -pod-network-cidr=192.168.0.0/22
 
 # Initialize Kubeadm For Current User
 mkdir -p $HOME/.kube
@@ -12,11 +12,9 @@ sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 # sudo kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
 kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.28.0/manifests/tigera-operator.yaml
-curl https://raw.githubusercontent.com/projectcalico/calico/v3.28.0/manifests/custom-resources.yaml -O
-kubectl create -f custom-resources.yaml
-kubectl get pods -n calico-system
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.28.0/manifests/custom-resources.yaml
 
-# sudo kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f https://docs.projectcalico.org/v3.14/manifests/calico.yaml
+# sudo kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f https://docs.projectcalico.org/manifests/calico.yaml
 
 # Install helm
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
