a3927043ef4aebfe3be5a705421a3481c2775450
MaxU1301 <max@ullrichfamily.com>
2024-06-04 21:18:16 -0400

    added JupyterHub config

---
+++ SubScripts/InstallJupyterHub.sh
@@ -0,0 +1,15 @@
+# Download config.yaml
+mkdir jupyterhub
+cd jupyterhub
+wget https://raw.githubusercontent.com/MaxU1301/Proxmox-Scripts/main/SubScripts/JupyterHubConfig/config.yaml
+
+# Install jupyterhub
+helm repo add jupyterhub https://hub.jupyter.org/helm-chart/
+helm repo update
+
+helm upgrade --cleanup-on-fail \
+  --install jupyterhub jupyterhub/jupyterhub \
+  --namespace jupyterhub \
+  --create-namespace \
+  --version=3.3.7
+  --values config.yaml
