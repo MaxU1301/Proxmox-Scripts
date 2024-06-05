cc9ac6bd13be7637b6d635a8e01ccef80245efc7
MaxU1301 <max@ullrichfamily.com>
2024-06-04 21:25:56 -0400

    added JupyterHub config

--- SubScripts/SetAsK8sMaster.sh
+++ SubScripts/SetAsK8sMaster.sh
@@ -22,9 +22,11 @@ curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 # Download MetalLB and NFS Scripts
 wget https://raw.githubusercontent.com/MaxU1301/Proxmox-Scripts/main/SubScripts/SetupMetalLB.sh
 wget https://raw.githubusercontent.com/MaxU1301/Proxmox-Scripts/main/SubScripts/SetupNFSProvisioner.sh
+wget https://raw.githubusercontent.com/MaxU1301/Proxmox-Scripts/main/SubScripts/InstallJupyterHub.sh
 
 sudo chmod +x SetupMetalLB.sh
 sudo chmod +x SetupNFSProvisioner.sh
+sudo chmod +x InstallJupyterHub.sh
 
 echo "Please run SetupNFSProvisioner.sh and SetupMetalLB.sh"
 kubeadm token create --print-join-command
