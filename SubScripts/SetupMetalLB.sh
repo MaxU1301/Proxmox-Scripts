21f613b0842e56215653cd6efff9a07b2705db8e
MaxU1301 <max@ullrichfamily.com>
2024-06-04 20:54:38 -0400

    updated metallb config

--- SubScripts/SetupMetalLB.sh
+++ SubScripts/SetupMetalLB.sh
@@ -3,10 +3,4 @@
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.14.5/config/manifests/metallb-native.yaml
 
 # Config for MetalLB
-mkdir MetalLBconfig
-cd MetalLBconfig
-wget https://raw.githubusercontent.com/MaxU1301/Proxmox-Scripts/main/SubScripts/MetalLBconfig/metallb.yaml
-
-sudo nano metallb.yaml
-
-kubectl create -f metallb.yaml
+kubectl create -f https://raw.githubusercontent.com/MaxU1301/Proxmox-Scripts/main/SubScripts/MetalLBconfig/metallb.yaml
