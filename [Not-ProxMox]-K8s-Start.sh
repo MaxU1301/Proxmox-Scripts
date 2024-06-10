378d3ea409d7ca6ba3bccbf03ce236cceb1c9cc6
MaxU1301 <max@ullrichfamily.com>
2024-06-10 11:20:40 -0400

    install and set dockerd

--- [Not-ProxMox]-K8s-Start.sh
+++ [Not-ProxMox]-K8s-Start.sh
@@ -20,7 +20,33 @@ ufw disable
 swapoff -a
 sed -i '/swap/d' /etc/fstab
 
-apt install -y docker.io apt-transport-https curl ca-certificates gpg
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Install cri-dockerd
+wget https://github.com/Mirantis/cri-dockerd/releases/download/v0.3.14/cri-dockerd-0.3.14.amd64.tgz
+tar xvf cri-dockerd-0.3.14.amd64.tgz
+./cri-dockerd/cri-dockerd
+
+rm -rf cri-dockerd
+rm cri-dockerd-0.3.14.amd64.tgz
+
+apt install -y apt-transport-https curl ca-certificates gpg
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
