6c991dfa0d7208d23836b011e93d4d886e80efec
MaxU1301 <max@ullrichfamily.com>
2024-06-11 14:33:46 -0400

    added some k3s setup scripts

--- SubScripts/k3s-Install/k3s-NGINX-Setup.sh
+++ SubScripts/k3s-Install/k3s-NGINX-Setup.sh
@@ -1,4 +1,5 @@
 sudo apt update
 sudo apt install -y nginx
 sudo rm /etc/nginx/nginx.conf
-sudo curl https://raw.githubusercontent.com/MaxU1301/Proxmox-Scripts/main/SubScripts/k3s-Install/k3s-nginx.conf -o nginx.conf
+sudo curl https://raw.githubusercontent.com/MaxU1301/Proxmox-Scripts/main/SubScripts/k3s-Install/k3s-nginx.conf -o /etc/nginx/nginx.conf
+sudo systemctl reload nginx
