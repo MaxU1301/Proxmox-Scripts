348fea15caceba70b1921a110851cd757c812590
MaxU1301 <max@ullrichfamily.com>
2024-06-12 14:24:30 -0400

    Updated UpdateJupyterHub.sh

--- SubScripts/JupyterHubConfig/UpdateJupyterHub.sh
+++ SubScripts/JupyterHubConfig/UpdateJupyterHub.sh
@@ -1,6 +1,16 @@
+# Update Config File
+sudo rm config.yaml
+wget https://raw.githubusercontent.com/MaxU1301/Proxmox-Scripts/main/SubScripts/JupyterHubConfig/config.yaml
+
+# Update JupyterHub
 sudo helm upgrade --cleanup-on-fail \
   jupyterhub jupyterhub/jupyterhub \
   --namespace jupyter-hub \
   --version=3.3.0 \
   --timeout 10m0s \
   --values config.yaml
+
+# Update Update script
+sudo rm UpdateJupyterHub.sh
+wget https://raw.githubusercontent.com/MaxU1301/Proxmox-Scripts/main/SubScripts/JupyterHubConfig/UpdateJupyterHub.sh
+sudo chmod +x UpdateJupyterHub.sh
