b5ddd70c59e19d50a8d828fc4a7ea46cf927ca09
MaxU1301 <max@ullrichfamily.com>
2024-06-11 22:23:12 -0400

    changed docker imgae and added ssl

--- SubScripts/JupyterHubConfig/UpdateJupyterHub.sh
+++ SubScripts/JupyterHubConfig/UpdateJupyterHub.sh
@@ -1,6 +1,6 @@
 helm upgrade --cleanup-on-fail \
   jupyterhub jupyterhub/jupyterhub \
-  --namespace default \
+  --namespace jupyter-hub \
   --version=3.3.0 \
   --timeout 10m0s \
   --values config.yaml
