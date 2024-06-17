70ba1181729718d24ad7d6daaf528c5f5b7644e5
MaxU1301 <max@ullrichfamily.com>
2024-06-17 12:56:02 -0400

    testing secret reading

--- SubScripts/JupyterHubConfig/UpdateJupyterHub.sh
+++ SubScripts/JupyterHubConfig/UpdateJupyterHub.sh
@@ -2,6 +2,16 @@
 sudo rm config.yaml
 wget https://raw.githubusercontent.com/MaxU1301/Proxmox-Scripts/main/SubScripts/JupyterHubConfig/config.yaml
 
+client_secret_secret="$(grep -n "client_secret" secrets.yaml | head -n 1 | cut -d: -f1)"
+client_id_secret="$(grep -n "client_id" secrets.yaml | head -n 1 | cut -d: -f1)"
+
+client_secret_config="$(grep -n "client_secret" config.yaml | head -n 1 | cut -d: -f1)"
+client_id_config="$(grep -n "client_id" config.yaml | head -n 1 | cut -d: -f1)"
+
+client_secret="$(sed -n "$client_secret_secret"p)"
+
+echo client_secret
+
 sudo rm monitorJupyterHub.sh
 wget https://raw.githubusercontent.com/MaxU1301/Proxmox-Scripts/main/SubScripts/JupyterHubConfig/monitorJupyterHub.sh
 chmod +x monitorJupyterHub.sh
