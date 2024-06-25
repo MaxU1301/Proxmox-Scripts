4f2188120c323957b728222f16f23eb14b06c77e
MaxU1301 <max@ullrichfamily.com>
2024-06-25 10:31:40 -0400

    Added High Availability K3S Setup

---
+++ SubScripts/k3s-Install/HA-k3s-JupyterHub-Setup.sh
@@ -0,0 +1,55 @@
+# Install Helm
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+
+# Setup nfs provisioner
+helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner/
+helm install nfs-subdir-external-provisioner nfs-subdir-external-provisioner/nfs-subdir-external-provisioner \
+    --set nfs.server=192.168.1.8 \
+    --set nfs.path=/mnt/Ptonomy/k8s-NFS-test \
+    --set storageClass.archiveOnDelete=false \
+    --set storageClass.defaultClass=false \
+    --set storageClass.name=nfs-client \
+    --set storageClass.accessModes=ReadWriteOnce
+
+# Install Rancher
+# helm repo add rancher-latest https://releases.rancher.com/server-charts/latest
+# kubectl create namespace cattle-system
+
+# kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.15.0/cert-manager.crds.yaml
+# helm repo add jetstack https://charts.jetstack.io
+# helm repo update
+# helm install cert-manager jetstack/cert-manager \
+#   --namespace cert-manager \
+#   --create-namespace \
+#   --set installCRDs=true
+
+# helm install rancher rancher-latest/rancher \
+#   --namespace cattle-system \
+#   --set hostname=rancher.tothemax.pro \
+#   --set bootstrapPassword=admin
+
+# kubectl -n cattle-system rollout status deploy/rancher
+# kubectl expose deployment rancher --name rancher-lb --port=443 --type=LoadBalancer -n cattle-system service/rancher-lb exposed
+
+# Setup Jupyterhub
+mkdir jupyterhub
+cd jupyterhub
+wget https://raw.githubusercontent.com/MaxU1301/Proxmox-Scripts/main/SubScripts/JupyterHubConfig/config.yaml
+wget https://raw.githubusercontent.com/MaxU1301/Proxmox-Scripts/main/SubScripts/JupyterHubConfig/secrets.yaml
+wget https://raw.githubusercontent.com/MaxU1301/Proxmox-Scripts/main/SubScripts/JupyterHubConfig/UpdateJupyterHub.sh
+chmod +x UpdateJupyterHub.sh
+
+# Install jupyterhub
+helm repo add jupyterhub https://hub.jupyter.org/helm-chart/
+helm repo update
+
+helm upgrade --cleanup-on-fail \
+  --install jupyterhub jupyterhub/jupyterhub \
+  --namespace jupyter-hub \
+  --create-namespace \
+  --version=3.3.0 \
+  --timeout 10m0s \
+  --values config.yaml
+
+# Monitor Jupyter Hub
+watch -n 0.1 kubectl get all -n jupyter-hub -o wide
--- [Multipass]-K3s-Cluster.bash
+++ [Multipass]-K3s-Cluster.bash
@@ -1,4 +1,6 @@
-multipass launch --name k3s-m1 --network Ethernet --cpus 4 --disk 20G --memory 2G
-multipass launch --name k3s-n1 --network Ethernet --cpus 4 --disk 20G --memory 2G
-multipass launch --name k3s-n2 --network Ethernet --cpus 4 --disk 20G --memory 2G
-multipass launch --name k3s-n3 --network Ethernet --cpus 4 --disk 20G --memory 2G
+multipass launch --name k3s-m1 --network Ethernet --cpus 4 --disk 30G --memory 2G
+multipass launch --name k3s-m3 --network Ethernet --cpus 4 --disk 30G --memory 2G
+multipass launch --name k3s-m2 --network Ethernet --cpus 4 --disk 30G --memory 2G
+multipass launch --name k3s-n1 --network Ethernet --cpus 4 --disk 30G --memory 2G
+multipass launch --name k3s-n2 --network Ethernet --cpus 4 --disk 30G --memory 2G
+multipass launch --name k3s-admin --network Ethernet --cpus 4 --disk 30G --memory 2G
